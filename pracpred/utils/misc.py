1f42639e7717bbc5e4fa71cad861d1683b58503f
Practically Predictable <practicallypredictable@practicallypredictable.com>
2017-12-21 21:38:06 -0500

    Add files via upload

---
+++ pracpred/utils/misc.py
@@ -0,0 +1,7 @@
+from inspect import getsourcelines
+
+def printsource(func):
+    """Print out the source code of a function."""
+    lines, _ = getsourcelines(func)
+    for line in lines:
+        print(line, end='')
